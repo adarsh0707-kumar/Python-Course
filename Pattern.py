118cdfc456e11fd8c4e9344043aafbef067dc005
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-07 21:37:59 +0530

    Add new patterns including Left Pascal Triangle, Right Pascal Triangle, Butterfly, Sandglass, and Hollow patterns in Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -111,3 +111,82 @@ for x in range(n):
     print("*", end=" ")
   print()
 """
+
+# Left Pascal Triangle
+"""
+for i in range(n-1):
+  for j in range(i+ 1):
+    print("*", end=" ")
+  print()
+for i in range(n):
+  for j in range(i, n):
+    print("*", end=" ")
+  print()
+
+"""
+# Right Pascal Triangle
+"""
+for i  in range(n-1):
+  for j in range(i,n):
+    print(" ", end=" ")
+  for j in range(i+1):
+     print("*", end=" ")
+  print()
+for i in range(n):
+  for j in range(i+1):
+    print(" ", end=" ")
+  for i in range(i, n):
+    print("*", end=" ")
+  print()
+
+"""
+
+# Butterfly pattern
+"""
+
+for i in range(n-1):
+  for j in range(i+1):
+    print("*", end=" ")
+  for j in range(i,n-2):
+    print(" ", end=" ")
+  for j in range(i,n-1):
+    print(" ", end=" ")
+  for j in range(i+1):
+    print("*", end=" ")
+  print()
+for i in range(n+4):
+  print("*", end=" ")
+print()
+for i in range(n-1):
+  for j in range(i,n-1):
+    print("*", end=" ")
+  for j in range(i):
+    print(" ", end=" ")
+  for j in range(i+1):
+    print(" ", end=" ")
+  for j in range(i,n-1):
+    print("*", end=" ")
+  print()
+
+"""
+
+# Sandglass pattern
+"""
+
+for i in range(n):
+  for j in range(i+1):
+    print("", end=" ")
+  for j in range(i,n):
+    print("*", end=" ")
+  print()
+for i in range(n-1):
+  for j in range(i+1,n):
+    print("", end=" ")
+  for j in range(i+2):
+    print("*", end=" ")
+  print()
+
+"""
+
+# Hollow Pattern
+
